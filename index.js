98c21f95ddbd5b0b4f557729dca697770d7e5e15
Drew Anderson <drw.a@outlook.com>
2017-09-23 11:15:10 -0400

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+let regularExpressions = {
+    // http://regexlib.com/REDetails.aspx?regexp_id=1574
+    stateLetters: /^(?:(A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|P[AR]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]))$/
+}
+
+module.exports = regularExpressions
